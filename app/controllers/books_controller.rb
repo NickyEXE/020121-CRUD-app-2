35d356acad55f7d02bbc81ad073c5f633f77e8de
NickyEXE <dovern42@gmail.com>
2021-06-11 14:21:12 -0400

    add full CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,39 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.create(book_params)
+    redirect_to book
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
   end
 
   private
 
   def set_book
     @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :genre, :author, :year_published)
   end
 
 end
